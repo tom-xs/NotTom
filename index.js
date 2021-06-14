0c63b72f4d56c0ef4c7d36d3457cc2ca55457e21
Tomas Xavier Santos <txs@ecomp.poli.br>
2021-06-14 12:22:44 -0300

    added telegram-bot-api package and initial implementations

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 require('dotenv').config();
 
 const Discord = require("discord.js");
+const TelegramBot = require('node-telegram-bot-api');
 
 const client = new Discord.Client();
+
+const bot = new TelegramBot(process.env.TelegramTOKEN,{polling: true});
+
+
 client.login(process.env.DiscordTOKEN);
 
 const prefix = './';
 
+//________começo do telegram________
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; 
+
+  bot.sendMessage(chatId, resp);
+});
+
+
+//_______ começo do discord_________
 client.on("message", function(message){
     if(message.author.bot) return;
     if(!message.content.startsWith(prefix)) return;
