2d70baa0e21f04f16c02925ffba89e3b16cef0aa
Tomas Xavier Santos <txs@ecomp.poli.br>
2021-06-25 17:23:22 -0300

    improved readability and added fowardMessage function

--- index.js
+++ index.js
@@ -25,45 +25,62 @@ var channelID = "797398583338205194";
 
 //________começo do telegram________
 //metodo para repassar texto
-//TODO: descobrir como hospedar o bot e manter ele ativo
-bot.on("text", msg => {
 
-	//define canal do discord que a mensagem será mandada
+//repassa mensagem para o canal do discord com id especificado
+function fowardMessage(msg, channelID){
+	//define o objeto canal com o respectivo ID
 	channel = client.channels.cache.get(channelID);
 
+	//trata username
+	const username = msg.from.username == undefined ? msg.from.first_name : msg.from.username;
+
+
 	//formatação da data
-	const d = new Date(msg.date);
+	const d = new Date(msg.date*1000);
 	const dateFormat = formatDate(d);
 
-	//envia mensagem
-	var reply = msg.from.username + " [" + dateFormat + "]: " + msg.text;
-	channel.send(reply);
 
+	//repassa mensagem
+	
+	if("text" in msg){
+		//envia mensagem
+		var reply = username + " [" + dateFormat + "]: " + msg.text;
+		channel.send(reply);
+	}else{
+		if("photo" in msg){
+			//adiciona legenda para imagem
+			legenda = msg.caption;
+
+			//pega o id da imagem com melhor resolução
+			image_id = msg.photo.slice(-1).pop().file_id;
+
+			//data e hora da mensagem
+			const d = new Date(msg.date*1000);
+			const dateFormat = formatDate(d);
+
+			//gera e envia imagem para discord
+			bot.getFileLink(image_id).then(fileLink => {
+				reply = username + " [" + dateFormat + "]: " + (legenda == undefined ? "" : legenda);
+				channel.send(reply, { files: [fileLink] });
+			});			
+
+			console.log("dale");
+		}
+	}
+
+
+}
+
+bot.on("text", msg => {
+	console.log(msg);
+	fowardMessage(msg,channelID);
 });
 
 //metodo para repassar imagens
-bot.on("photo", file => {
-	console.log(file);
-	//define o canal do disc que a imagem será mandada
-	channel = client.channels.cache.get(channelID);
-
-	//legenda da mensagem
-	legenda = file.caption;
-
-	//pega o id da imagem com melhor resolução
-	image_id = file.photo.slice(-1).pop().file_id;
-
-	//data e hora da mensagem
-	const d = new Date(file.date);
-	const dateFormat = formatDate(d);
-
-	//gera e envia imagem para discord
-	file_path = bot.getFileLink(image_id).then(fileLink => {
-		reply = file.from.username + " [" + dateFormat + "]: " + (legenda == undefined ? "" : legenda);
-		channel.send(reply, { files: [fileLink] });
-	});
-
+bot.on("photo", msg => {
+	fowardMessage(msg,channelID);
 })
+
 
 //_______ começo do discord_________
 client.on("message", function (message) {
